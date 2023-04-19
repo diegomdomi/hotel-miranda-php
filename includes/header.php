1959c850349a4541af458b2346f180709f832b1f
diegomdomi <diegomdomi@gmail.com>
2023-04-19 10:22:42 +0200

    adding Assets folder

--- includes/header.php
+++ includes/header.php
@@ -18,7 +18,7 @@
                 <div class="container-menu__burger"><img src="./Assets/1.png"></div>
                 <div class="container-menu__burger-close"><img src="./Assets/icon/cerrar.png"></div>
             </div>
-            <a href="./index.html">
+            <a href="./index.php">
                 <div class="container-menu__box-h"><p>H</p></div>
             </a>
                 <div class="container-menu__main-title"><span class="container__title">HOTEL</span><p>MIRANDA</p></div>
@@ -27,8 +27,8 @@
         </div>
     </header>
     <div class="nav__list ">
-        <a class="a-menu" href="./about-us.html">About Us</a>
-        <a class="a-menu" href="./rooms.html">Rooms</a>
-        <a class="a-menu" href="./offers.html">Offers</a>
-        <a class="a-menu" href="./contact.html">Contact</a>
+        <a class="a-menu" href="./about-us.php">About Us</a>
+        <a class="a-menu" href="./rooms.php">Rooms</a>
+        <a class="a-menu" href="./offers.php">Offers</a>
+        <a class="a-menu" href="./contact.php">Contact</a>
     </div>
