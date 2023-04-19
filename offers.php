704708f03fcca21d4a9df3f1ece7bf28d6acc6c0
diegomdomi <diegomdomi@gmail.com>
2023-04-19 09:44:19 +0200

    offers.php

---
+++ offers.php
@@ -0,0 +1,168 @@
+<?php include './includes/header.php';
+?>
+ <!-- about-Us -->
+  <div class="about-us__home">
+        <p>THE ULTIMATE LUXURY</p>
+        <h1>Our Offers</h1>
+        <div class="about-us__home-about"><h6>Home | <span>Offers</span></h6></div>
+    </div>
+    <div class="offers">
+        <div class="offers__prices">
+            <img src="./Assets/img-room/room1.jpg" alt="room-luxury">
+            <div class="offers__prices-text">
+                <h5>$500<sub>/Night</sub></h5>
+                <h5>$345<sub>/Night</sub></h5>
+            </div>
+        </div>
+        <h6>DOUBLE BED</h6>
+        <h1 class="offers__title">Luxury Double Bed</h1>
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehend erit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+    </div>
+    <section class="amenities amenities-offers">
+            <div class="amenities__list">
+                <div class="amenities__list__1">
+                    <p class="amenities__list__p"><img src="./Assets/icon/air-conditioner.png" alt="">Air conditioner</p>
+                    <p class="amenities__list__p"><img src="./Assets/icon/breakfast.png" alt="">Breakfast</p>
+                    <p class="amenities__list__p"><img src="./Assets/icon/cleaning.png" alt="">Cleaning</p>
+                    <p class="amenities__list__p"><img src="./Assets/icon/grocery.png" alt="">Grocery</p>
+                    <p class="amenities__list__p"><img src="./Assets/icon/shop-near.png" alt="">Shop near/p>
+                </div>
+                <div class="amenities__list__2">  
+                    <p  class="amenities__list__p"><img src="./Assets/icon/wifi.png" alt="">High speed WiFi</p>
+                    <p  class="amenities__list__p"><img src="./Assets/icon/kitchen.png" alt="">Kitchen</p>
+                    <p  class="amenities__list__p"><img src="./Assets/icon/shower.png" alt="">Shower</p>
+                    <p  class="amenities__list__p"><img src="./Assets/icon/single-bed.png" alt="">Single bed</p>
+                    <p  class="amenities__list__p"><img src="./Assets/icon/towel.png" alt="">Towels</p>
+                </div>
+            </div>
+            <button class="book-now">BOOK NOW</button>
+    </section>
+    <div class="offers">
+        <div class="offers__prices">
+            <img src="./Assets/img-room/room1.jpg" alt="room-luxury">
+            <div class="offers__prices-text">
+                <h5>$500<sub>/Night</sub></h5>
+                <h5>$345<sub>/Night</sub></h5>
+            </div>
+        </div>
+        <h6>DOUBLE BED</h6>
+        <h1 class="offers__title">Luxury Double Bed</h1>
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehend erit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+    </div>
+    <section class="amenities amenities-offers">
+            <div class="amenities__list">
+                <div class="amenities__list__1">
+                    <p class="amenities__list__p"><img src="./Assets/icon/air-conditioner.png" alt="">Air conditioner</p>
+                    <p class="amenities__list__p"><img src="./Assets/icon/breakfast.png" alt="">Breakfast</p>
+                    <p class="amenities__list__p"><img src="./Assets/icon/cleaning.png" alt="">Cleaning</p>
+                    <p class="amenities__list__p"><img src="./Assets/icon/grocery.png" alt="">Grocery</p>
+                    <p class="amenities__list__p"><img src="./Assets/icon/shop-near.png" alt="">Shop near/p>
+                </div>
+                <div class="amenities__list__2">  
+                    <p  class="amenities__list__p"><img src="./Assets/icon/wifi.png" alt="">High speed WiFi</p>
+                    <p  class="amenities__list__p"><img src="./Assets/icon/kitchen.png" alt="">Kitchen</p>
+                    <p  class="amenities__list__p"><img src="./Assets/icon/shower.png" alt="">Shower</p>
+                    <p  class="amenities__list__p"><img src="./Assets/icon/single-bed.png" alt="">Single bed</p>
+                    <p  class="amenities__list__p"><img src="./Assets/icon/towel.png" alt="">Towels</p>
+                </div>
+            </div>
+            <button class="book-now">BOOK NOW</button>
+    </section>
+    <div class="offers">
+        <div class="offers__prices">
+            <img src="./Assets/img-room/room1.jpg" alt="room-luxury">
+            <div class="offers__prices-text">
+                <h5>$500<sub>/Night</sub></h5>
+                <h5>$345<sub>/Night</sub></h5>
+            </div>
+        </div>
+        <h6>DOUBLE BED</h6>
+        <h1 class="offers__title">Luxury Double Bed</h1>
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehend erit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+    </div>
+    <section class="amenities amenities-offers">
+            <div class="amenities__list">
+                <div class="amenities__list__1">
+                    <p class="amenities__list__p"><img src="./Assets/icon/air-conditioner.png" alt="">Air conditioner</p>
+                    <p class="amenities__list__p"><img src="./Assets/icon/breakfast.png" alt="">Breakfast</p>
+                    <p class="amenities__list__p"><img src="./Assets/icon/cleaning.png" alt="">Cleaning</p>
+                    <p class="amenities__list__p"><img src="./Assets/icon/grocery.png" alt="">Grocery</p>
+                    <p class="amenities__list__p"><img src="./Assets/icon/shop-near.png" alt="">Shop near/p>
+                </div>
+                <div class="amenities__list__2">  
+                    <p  class="amenities__list__p"><img src="./Assets/icon/wifi.png" alt="">High speed WiFi</p>
+                    <p  class="amenities__list__p"><img src="./Assets/icon/kitchen.png" alt="">Kitchen</p>
+                    <p  class="amenities__list__p"><img src="./Assets/icon/shower.png" alt="">Shower</p>
+                    <p  class="amenities__list__p"><img src="./Assets/icon/single-bed.png" alt="">Single bed</p>
+                    <p  class="amenities__list__p"><img src="./Assets/icon/towel.png" alt="">Towels</p>
+                </div>
+            </div>
+            <button class="book-now">BOOK NOW</button>
+    </section>
+    <div class="offers">
+        <div class="offers__prices">
+            <img src="./Assets/img-room/room1.jpg" alt="room-luxury">
+            <div class="offers__prices-text">
+                <h5>$500<sub>/Night</sub></h5>
+                <h5>$345<sub>/Night</sub></h5>
+            </div>
+        </div>
+        <h6>DOUBLE BED</h6>
+        <h1 class="offers__title">Luxury Double Bed</h1>
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehend erit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+    </div>
+    <section class="amenities amenities-offers">
+            <div class="amenities__list">
+                <div class="amenities__list__1">
+                    <p class="amenities__list__p"><img src="./Assets/icon/air-conditioner.png" alt="">Air conditioner</p>
+                    <p class="amenities__list__p"><img src="./Assets/icon/breakfast.png" alt="">Breakfast</p>
+                    <p class="amenities__list__p"><img src="./Assets/icon/cleaning.png" alt="">Cleaning</p>
+                    <p class="amenities__list__p"><img src="./Assets/icon/grocery.png" alt="">Grocery</p>
+                    <p class="amenities__list__p"><img src="./Assets/icon/shop-near.png" alt="">Shop near/p>
+                </div>
+                <div class="amenities__list__2">  
+                    <p  class="amenities__list__p"><img src="./Assets/icon/wifi.png" alt="">High speed WiFi</p>
+                    <p  class="amenities__list__p"><img src="./Assets/icon/kitchen.png" alt="">Kitchen</p>
+                    <p  class="amenities__list__p"><img src="./Assets/icon/shower.png" alt="">Shower</p>
+                    <p  class="amenities__list__p"><img src="./Assets/icon/single-bed.png" alt="">Single bed</p>
+                    <p  class="amenities__list__p"><img src="./Assets/icon/towel.png" alt="">Towels</p>
+                </div>
+            </div>
+            <button class="book-now">BOOK NOW</button>
+    </section>
+
+    <div class="room-card card-background">
+        <h4>POPULAR LIST</h4>
+        <h2>Popular Rooms</h2>
+        <div class="room__slider card-background__slider">
+            <div class="swiper">
+            <div class="swiper-wrapper">
+              <!-- Slides -->
+              <div class="swiper-slide card-img"><img src="./Assets/img-room/room1.jpg" alt=""></div>
+            </div>
+            <!-- If we need pagination -->
+            <div class="swiper-pagination"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+          </div>
+    </div>
+    <div class="services">
+        <img src="./Assets/bed_icon.png" alt="bed"> 
+        <img src="./Assets/wifi_icon.png" alt="wifi"> 
+        <img src="./Assets/automobile_car_icon.png" alt="car"> 
+        <img src="./Assets/cold_icon.png" alt="cold"> 
+        <img src="./Assets/gym_icon.png" alt="gym"> 
+        <img src="./Assets/smoking_no_icon.png" alt="no_smokin"> 
+        <img src="./Assets/cocktail_drink.png" alt="cocktail"> 
+     </div>
+     <div class="card-info">
+        <h4>Minimal Duplex Room</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor .</p>
+        <div class="minimal-card__price">
+            <h5>$345<sub>/Night</sub></h5>
+            <h6>BOOking Now</h6>
+        </div>
+    </div>
+</div>
+<?php include './includes/footer.php';
+?>
